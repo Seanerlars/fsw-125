00d205716fcb429e8ed636129195b9d1ec33d8f3
Seanerlars <Sean.larsen@bryanuniversity.edu>
2021-01-25 21:15:25 -0600

    math

---
+++ week1/math/app.js
@@ -0,0 +1,9 @@
+const { sum, subtract, multiply, divide } = require("./math.js")
+
+console.log(sum(10, 9))
+console.log(subtract(10, 9))
+console.log(multiply(10, 9))
+console.log(divide(100, 5))
+
+
+
